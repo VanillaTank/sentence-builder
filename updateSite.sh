fbf1860eb83228744a5793785cb2890ec20bf0b1
Tatiana Mikhailova <tedien2016@yandex.ru>
2022-06-25 06:28:43 +0800

    try create site script

---
+++ updateSite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ng build --output-path docs --base-href https://vanillatank.github.io/sentence-builder/
+
+#ng build --base-href C:/Users/Tanya/Desktop/personal-project/sentence-builder/docs/ --cross-origin anonymous
+#
+#rm -R ./docs
+#echo 'docs folder deleted'
+#mkdir ./docs
+#echo 'docs folder created'
+#
+#cp ./dist/some-project/* ./docs
+#echo 'files are copied'
+
+#git add ./docs/*
+#git commit -m 'update site'
+#git push
+#echo 'site is pushed'
