b525ba9022ff612f77cc89205f63513ed0451273
Tatiana Mikhailova <tedien2016@yandex.ru>
2022-06-25 07:00:28 +0800

    update site

--- updateSite.sh
+++ updateSite.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 ng build --output-path docs --base-href https://vanillatank.github.io/sentence-builder/
-cp ./docs/index.html ./docs/404.html
-
-#rm -R ./docs
-#echo 'docs folder deleted'
-#mkdir ./docs
-#echo 'docs folder created'
-#
-#cp ./dist/some-project/* ./docs
-#echo 'files are copied'
+cp ./docs/index.html ./docs/404.html #Скопируй и переименуй файл. Хз зачем, так в доке.
 
 git add .
 git commit -m 'update site'
 git push
-echo 'site is pushed'
+echo 'site is updated'
