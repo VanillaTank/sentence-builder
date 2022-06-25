5d519954bde65bd71aacc8b6875b2ea63e8be816
Tatiana Mikhailova <tedien2016@yandex.ru>
2022-06-26 00:44:15 +0800

    add PAST_CONTINUOUS data

--- src/app/cards-service.service.ts
+++ src/app/cards-service.service.ts
@@ -4,9 +4,10 @@ import { PRESENT_SIMPLE } from './cards-data/PRESENT_SIMPLE';
 import { PAST_SIMPLE } from './cards-data/PAST_SIMPLE';
 import { PRESENT_CONTINUOUS } from './cards-data/PRESENT_CONTINIOUS';
 import { FUTURE_SIMPLE } from './cards-data/FUTURE_SIMPLE';
+import { PAST_CONTINUOUS } from './cards-data/PAST_CONTINUOUS'
 import { CONDITIONAL_CARDS } from './cards-data/CONDITION';
 import { GeneralSearchValues, ConditionSearchValues, SearchItem } from './filters/interfaces'
-import {GENERAL_SEARCH_ITEMS, CONDITION_SEARCH_ITEMS} from './filters-data/SEARCH_ITEMS'
+import { GENERAL_SEARCH_ITEMS, CONDITION_SEARCH_ITEMS } from './filters-data/SEARCH_ITEMS'
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class CardsService {
   // ADD NEW CARD DATA HERE
   GENERAL_CARDS: Card[] = [
     ...PRESENT_SIMPLE, ...PAST_SIMPLE, ...FUTURE_SIMPLE,
-    ...PRESENT_CONTINUOUS
+    ...PRESENT_CONTINUOUS, ...PAST_CONTINUOUS
   ];
   CONDITIONAL_CARDS: ConditionCard[] = CONDITIONAL_CARDS;
 
@@ -26,15 +27,21 @@ export class CardsService {
   selectedCards: Card[] | [] = this.activeCards;
   activeFilterName: string = "Generals";
 
-  constructor() { }
+  constructor() {
+  }
 
-  updateActiveFilter(choosenFilter: string):void {
+  updateActiveFilter(choosenFilter: string): void {
     this.activeFilterName = choosenFilter;
     switch (this.activeFilterName) {
-      case 'Generals': this.activeCards = this.GENERAL_CARDS; break;
-      case 'Conditional': this.activeCards = this.CONDITIONAL_CARDS; break;
+      case 'Generals':
+        this.activeCards = this.GENERAL_CARDS;
+        break;
+      case 'Conditional':
+        this.activeCards = this.CONDITIONAL_CARDS;
+        break;
       //ADD NEW FILTER DATA HERE
-      default: this.activeCards = this.GENERAL_CARDS;
+      default:
+        this.activeCards = this.GENERAL_CARDS;
     }
     this.actviveFilterItem().map(item => {
       item.values.map(it => it.checked = false)
@@ -42,27 +49,32 @@ export class CardsService {
     this.selectedCards = this.activeCards;
   }
 
-  getAllCards():void {
+  getAllCards(): void {
     this.selectedCards = this.activeCards;
   }
 
-  getActiveFilterName():string {
+  getActiveFilterName(): string {
     return this.activeFilterName;
   }
 
-  actviveFilterItem():SearchItem[]  {
+  actviveFilterItem(): SearchItem[] {
     switch (this.activeFilterName) {
-      case 'Generals': return GENERAL_SEARCH_ITEMS;
-      case 'Conditional': return CONDITION_SEARCH_ITEMS;
+      case 'Generals':
+        return GENERAL_SEARCH_ITEMS;
+      case 'Conditional':
+        return CONDITION_SEARCH_ITEMS;
       //TODO дописать сюда путь к элементам фильтра, когда будут данные
-      default: return GENERAL_SEARCH_ITEMS;
+      default:
+        return GENERAL_SEARCH_ITEMS;
     }
   }
 
-  updateSelectedCards(query:  GeneralSearchValues | ConditionSearchValues): void {
+  updateSelectedCards(query: GeneralSearchValues | ConditionSearchValues): void {
     this.selectedCards = this.activeCards.filter(card => {
-      for(let q in query) {
-        if (query[q].length > 0 && !query[q].includes(card.type[q])) { return false }
+      for (let q in query) {
+        if (query[q].length > 0 && !query[q].includes(card.type[q])) {
+          return false
+        }
       }
       return true;
     })
