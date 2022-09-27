5a21f3cfa0b16a448f0640fa689708bdcf39a259
VanillaTank <tedien2016@yandex.ru>
2022-09-28 04:54:07 +0800

    clean project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,5 @@ import { CardsService } from './cards-service.service';
 })
 export class AppComponent {
   title = 'some-project';
-  filterList: string[] = ['General', 'Conditional',
-    //'Questions', 'Would', 'Побудительные'
-  ];
-
-  activeFilter: string = this.filterList[0];
-
-  constructor(private cardsService: CardsService) {
-  }
-
-  onFilterListItemClick(chosenFilter: string) {
-    this.activeFilter = chosenFilter;
-    this.cardsService.updateActiveFilter(chosenFilter)
-  }
-
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardsPartComponent } from './cards-part/cards-part.component';
-import { FiltersComponent } from './filters/filters.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CardsPartComponent,
-    FiltersComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
